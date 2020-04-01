8431ccbe378ac3e31f0272adf5e075f83312d19b
valentin1991 <valentin.semenov.1991@mail.ru>
2020-04-01 18:47:07 +0300

    project

--- cut/models.py
+++ cut/models.py
@@ -6,8 +6,8 @@ from django.core.exceptions import ValidationError
 
 class ListLink(models.Model):
     avtor = models.ForeignKey(User, on_delete = models.CASCADE)
-    long_link = models.URLField(max_length=250, verbose_name = 'Длинная ссылка', unique=True)
-    short_link = models.CharField(max_length=100, verbose_name = 'Короткая ссылка')
+    long_link = models.URLField(max_length=250, verbose_name = 'Длинная ссылка')
+    short_link = models.CharField(max_length=100, verbose_name = 'Короткая ссылка', unique=True)
 
     def get_absolute_url(self):
         return reverse('createlink')
